62ef25ad47ff7b03c5c3e7e445199741b8340316
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-05-10 19:08:29 +0530

    intial.commit
    IT IS A JAVA DSA PROGRAM TO SOLVE A SORTING PROBLEM NAME AS MAXIMUM GUEST MEETING PROBLEM IN JAVA

---
+++ L33MaximumGuestProblem.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class L33MaximumGuestProblem {
+
+    static int maxGuest(int arr[], int dep[], int n){  // arr= arrivel , dep= departure
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int i=1 ,j=0, res=1, curr=1;
+        while(i<n && j<n){
+            if(arr[i]<= dep[j]){
+                curr++; i++;
+
+            }
+            else{
+                curr--;
+                j++;
+
+            }
+            res= Math.max(res, curr);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int arr[]={900,600,700};
+        int dep[]={1000, 800,730};
+        int n=3;
+        System.out.println(maxGuest(arr, dep, n));
+    }
+}
