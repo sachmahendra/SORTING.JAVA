a62a2eb512263c08d16e9de4281a54ea9adca316
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-05-19 20:37:48 +0530

    initial.commit
    IT IS A JAVA CODE IN NETWORK ANALYSIS FOR SERVER AND CLIENT MSG PROCESS

---
+++ A267ServerAndClientMsg.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class A267ServerAndClientMsg {
+    public static void main(String[] args)throws Exception {
+        
+        ServerSocket ss = new ServerSocket(2000);
+        Socket stk = ss.accept();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+
+        String msg;
+        StringBuilder sb;
+
+        do{
+
+           msg = br.readLine();
+
+           sb = new StringBuilder(msg);
+           sb.reverse();
+
+           msg = sb.toString();
+
+           ps.println(msg);
+
+        }while(!msg.equals("dne"));
+    }
+}
+
+ class Client {
+    public static void main(String[] args)throws Exception {
+        
+        Socket stk = new Socket("192.168.48.133",2000); // " number" is ip number of this laptop
+        
+        BufferedReader keyb = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+
+        String msg;
+        
+        do{
+
+           msg = keyb.readLine();
+           ps.println(msg);
+           msg=br.readLine();
+
+           System.out.println("From Server"+msg);
+
+        }while(!msg.equals("dne"));
+
+        stk.close();
+    }
+}
