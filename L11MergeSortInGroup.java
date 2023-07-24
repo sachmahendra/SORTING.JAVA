1e214cf34eee7a5be5a5ad782816329f38f7efdb
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-07-24 23:33:44 +0530

    initial.commit
    IT IS A JAVA SORTING CODE FOR IMPLEMENTING MERGE SORT IN GROUP

--- L11MergeSortInGroup.java
+++ L11MergeSortInGroup.java
@@ -21,7 +21,7 @@ public class L11MergeSortInGroup
         int[] left=new int[n1];int[]right=new int[n2];
         for(int i=0;i<n1;i++)
             left[i]=arr[i+l];
-        for(int j=0;j<n2;j++)
+        for(int j=0;j<n2;j++)   
             right[j]=arr[m+1+j];    
         int i=0,j=0,k=l;
         while(i<n1 && j<n2){
