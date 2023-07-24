aff5ff4aa309afae40e28dabf4f7d570cd8c8947
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-07-24 23:35:01 +0530

    initial.commit
    IT IS A CHANGE IN CODE WHICH IS FOR COORDINATE SORTHING IN JAVA

--- L3CoordinateSorting.java
+++ L3CoordinateSorting.java
@@ -1,23 +1,26 @@
-import java.util.Arrays;
+import java.util.*;
 
-class Point implements Comparable<Point>{
+class Point implements Comparable<Point> {
     int x;
-    int y ;
-    Point(int x, int y){
-        this.x= x;
-        this.y= y;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    public int compareTo(Point P){
+
+    public int compareTo(Point P) {
         return this.x - P.x;
     }
 }
+
 public class L3CoordinateSorting {
     public static void main(String[] args) {
-        Point arr[] ={new Point(10, 12), new Point(3,14), new Point(24,3)};
+        Point arr[] = { new Point(10, 12), new Point(3, 14), new Point(24, 3) };
 
         Arrays.sort(arr);
-        for(int i=  0;i< arr.length; i++ ){
-            System.out.println(arr[i].x + " "+ arr[i].y);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i].x + " " + arr[i].y);
         }
     }
 }
